35d261582e818b8ae2e5d6b7f81ef6174d911dd6
Justin Wei <justin.lj.wei@gmail.com>
2016-12-21 15:37:49 -0800

    initial commit

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function sayHello() {
+	var name = document.getElementById('Name').value;
+	var content = document.getElementById('Hello');
+	console.log(name);
+	if (name == "") {
+		content.innerHTML = "Please enter a name above!";
+	}
+	else {
+		content.innerHTML = "Welcome, " + name + "!";
+	}
+}
+
+function changeContent() {
+	var content = document.getElementById(elementID);
+	content.innerHTML = text;
+}
